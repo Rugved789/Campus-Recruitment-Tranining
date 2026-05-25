41523580962d576a4d08c107dc7b1b74fa855706
Rugved789 <rugved.gadge.789@gmail.com>
2026-05-25 13:30:53 +0530

    Day1

--- DSA_C/nestingloops.c
+++ DSA_C/nestingloops.c
@@ -2,12 +2,13 @@
 
 int main()
 {
-    
+    int num=9;
         for (int j=1;j<=3;j++){
-            for(int k = 1;k<=3;k++){
-                printf("%d ",j);
+            for(int k=1;k<=3;k++){
+                printf("%d ",num);
+                num--;
             }
-                    printf("\n");
+            printf("\n");
         }
 
     return 0;
---
+++ DSA_C/pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int num=1;
+        for (int i=1;i<=3;i++){
+            for(int j=1;j<=3;j++){
+                if(num%2==0){
+                    printf("%d",num);
+                }
+                else{
+                    printf("_");
+                }
+                num++;
+            }
+            printf("\n");
+        }
+
+    return 0;
+
+}
