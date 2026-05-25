120e107b9141241128e0a0f06e6b779aab90ba7f
Rugved789 <rugved.gadge.789@gmail.com>
2026-05-25 11:01:31 +0530

    Day1

---
+++ DSA_C/controlStatements.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+    int a = 10;
+    int b = 20;
+
+    printf("%d\n",a);
+    printf("%d\n",b);
+
+
+    if(a>b){
+        printf("A is Greater");
+    }
+    else{
+        printf("B is Greater");
+    }
+
+
+}
---
+++ DSA_C/datatypes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+    int a = 10;
+    float b=3.14;
+    char ch = 'R';
+
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%c\n",ch);
+
+}
