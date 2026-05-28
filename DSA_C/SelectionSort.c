bdfc70b1f14d87cd7a0400ed7d37e24d42d1a3aa
Rugved789 <rugved.gadge.789@gmail.com>
2026-05-28 10:27:37 +0530

    Day4

---
+++ DSA_C/SelectionSort.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int sort (int arr[],int size){
+    for(int i=0;i<size-1;i++){
+        int min=i;
+        for(int j=i+1;j<size;j++){
+            if(arr[j]<arr[min]){
+                int temp = arr[j];
+                arr[j]=arr[min];
+                arr[min]=temp;
+            }
+
+        }
+    }
+    return 0;
+}
+void printarray(int arr[],int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+}
+
+int main(){
+
+
+    int arr[]={5,7,1,2,18,24,16,15,30,25,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Original Array:");
+    for(int i=0;i<size;i++){
+        printf("%d ",arr[i]);
+    }
+
+    sort(arr,size);    
+    printf("\n");
+    printf("Sorted Array:");
+    printarray(arr,size);
+    return 0;
+}
