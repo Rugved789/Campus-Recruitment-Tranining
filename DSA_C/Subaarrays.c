f03fc2f0ad7d83871f71952c96d2c346dfc12da5
Rugved789 <rugved.gadge.789@gmail.com>
2026-05-29 11:42:00 +0530

    Day5

---
+++ DSA_C/Subaarrays.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    for(int start = 0; start<size;start++){
+        for(int end = start;end<size;end++){
+            for(int i = start;i<=end;i++){
+                printf()
+            }
+        }
+    }
+    return 0;
+}
