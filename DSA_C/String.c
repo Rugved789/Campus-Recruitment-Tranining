ed8b7c794ebfae1ff917fac4c801b58a3e9ced03
Rugved789 <rugved.gadge.789@gmail.com>
2026-06-02 11:01:36 +0530

    Day7

--- DSA_C/String.c
+++ DSA_C/String.c
@@ -5,13 +5,11 @@ int main(){
     char c = '&';
     printf("%c\n",c);
     char arr[]="Hello";
-    int count =0;
-    while(arr[count]!='\0'){
-        count++;
-    }
 
-    for (int i=0;i<count;i++){
-        printf("%c\t",arr[i]);
+    int strLength= strlen(arr);
+
+    for (int i=strLength;i>=0;i--){
+        printf("%c",arr[i]);
     }
     return 0;
 }
