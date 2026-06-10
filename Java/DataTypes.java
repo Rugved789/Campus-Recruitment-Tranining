515dc825681bcab8164b8b5962150fd0e61046b1
Rugved789 <rugved.gadge.789@gmail.com>
2026-06-10 10:52:16 +0530

    Day11

---
+++ Java/DataTypes.java
@@ -0,0 +1,20 @@
+class DataTypes {
+    public static void main(String[] args) {
+        // Primitive data types
+        int myInt = 10; // Integer
+        double myDouble = 3.14; // Floating-point number
+        char myChar = 'A'; // Character
+        boolean myBoolean = true; // Boolean
+
+        // Non-primitive data types (Objects)
+        String myString = "Hello, World!"; // String
+
+        // Output the values
+        System.out.println("Integer: " + myInt);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Character: " + myChar);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("String: " + myString);
+
+    }
+}
