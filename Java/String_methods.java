03c44323254f1116cf405966a0e250f718508ea4
Rugved789 <rugved.gadge.789@gmail.com>
2026-06-11 10:41:12 +0530

    Day12

--- Java/String_methods.java
+++ Java/String_methods.java
@@ -16,5 +16,13 @@ public class String_methods {
         System.out.println(str1.charAt(2));
         System.out.println(str1.equals(str2));
         System.out.println(str1 == str2);
+
+        for(int i = str1.length()-1;i>=0;i--)
+            System.out.print(str1.charAt(i));
+        System.out.println();
+
+        String str3 = new StringBuffer(str1).reverse().toString();
+        System.out.println(str3);
+        
     }
 }
